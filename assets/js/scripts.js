6ce59ac1de759f348a21f098e3644c625be90ee9
Alexander Persky <info@alexanderpersky.com>
2016-07-03 23:59:19 -0700

    added basic functionality for an accordion dropdown for sub-menus

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,6 +1,13 @@
 $(function() {
+
   $('#menu-btn').click(function() {
     $(this).toggleClass('active');
     $('#overlay').toggleClass('open');
   });
+
+  $('.has-children').children('a').on('click', function(event){
+    event.preventDefault();
+    $(this).toggleClass('submenu-open').next('.sub-menu').slideToggle(200).end().parent('.has-children').siblings('.has-children').children('a').removeClass('submenu-open').next('.sub-menu').slideUp(200);
+  });
+
 });
