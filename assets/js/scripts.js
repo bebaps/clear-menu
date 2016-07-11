8fcbe39ab1ff4aa95d5b9cb0c17ee90c5bcc2277
Alexander Persky <info@alexanderpersky.com>
2016-07-11 08:06:43 -0700

    add a TODO

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -5,6 +5,7 @@ $(function() {
     $('#overlay').toggleClass('open');
   });
 
+  // TODO: can this be simplified with some CSS? Not rely on the jQuery animations?
   $('.has-children').children('a').on('click', function(event){
     event.preventDefault();
     $(this).toggleClass('submenu-open').next('.sub-menu').slideToggle(200).end().parent('.has-children').siblings('.has-children').children('a').removeClass('submenu-open').next('.sub-menu').slideUp(200);
