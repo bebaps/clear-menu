7b9e2fe8e1e42e7f9a0f9d04282d6b0b79d457aa
Alexander Persky <info@alexanderpersky.com>
2016-08-17 21:48:49 -0700

    Edit the JS, turning it into a jQuery plugin

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,16 +1,33 @@
+// TODO: inject a button to serve as the submenu toggle (if hasSubmenu = true)
+// TODO: inject proper CSS class to control the direction of the overlay reveal (reveal: top, bottom, left, right, fade)
+
+
+$.fn.clearmenu = function(options) {
+  var settings = $.extend({
+    trigger: this,
+    overlay: $('#cm-overlay'),
+    hasSubmenu: true,
+    submenu: $('.cm-has-submenu'),
+    reveal: 'top'
+  }, options);
+
+  var revealMenu = function() {
+    settings.trigger.toggleClass('cm-is-active');
+    settings.overlay.toggleClass('cm-is-visible');
+  };
+
+  settings.trigger.on('click.clearmenu', revealMenu);
+
+  if (settings.hasSubmenu) {
+    var toggleSubMenu = function() {
+      $(this).parent().toggleClass('cm-has-submenu-open').find('ul').slideToggle(200);
+    };
+
+    settings.submenu.on('click.clearmenu', '.cm-sub-trigger', toggleSubMenu);
+  }
+}
+
+
 $(function() {
-    var hamburger = $('#cm-hamburger'),
-        overlay = $('#cm-overlay');
-
-  $(hamburger).on('click', function() {
-    hamburger.toggleClass('cm-is-active');
-    overlay.toggleClass('cm-is-visible');
-  });
-
-  // TODO: can this be simplified with some CSS? Not rely on the jQuery animations?
-  // TODO: inject a button to serve as the toggle. Click on the button to activate the behavior
-  $('.cm-has-submenu').on('click', '.cm-sub-trigger', function(){
-    $(this).parent().toggleClass('cm-has-submenu-open').find('ul').slideToggle(200).end().parent('.cm-has-submenu').siblings('.cm-has-submenu').removeClass('cm-has-submenu-open').next('ul').slideUp(200);
-  });
-
+  $('#cm-hamburger').clearmenu();
 });
