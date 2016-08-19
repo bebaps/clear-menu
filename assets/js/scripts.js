d54d2e6b77e466768322743e20dec93cc74d3829
Alexander Persky <info@alexanderpersky.com>
2016-08-18 21:14:22 -0700

    A few small changes to the JS functionality, but still more to do

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -3,12 +3,27 @@
 
 
 $.fn.clearmenu = function(options) {
+  var classes = {
+    nav: '.cm-nav',
+    listItem: '.cm-has-submenu',
+    hasSubmenuOpen: '.cm-has-submenu-open',
+    subMenuTrigger: '.cm-sub-trigger',
+    overlay: '.cm-overlay',
+    top: 'cm-reveal-top',
+    bottom: 'cm-reveal-bottom',
+    left: 'cm-reveal-left',
+    right: 'cm-reveal-right',
+    fade: 'cm-reveal-fade',
+    visible: '.cm-is-visible'
+  };
+
   var settings = $.extend({
     trigger: this,
     overlay: $('#cm-overlay'),
     hasSubmenu: true,
     submenu: $('.cm-has-submenu'),
-    reveal: 'top'
+    reveal: 'top',
+    wordpress: false
   }, options);
 
   var revealMenu = function() {
@@ -19,11 +34,13 @@ $.fn.clearmenu = function(options) {
   settings.trigger.on('click.clearmenu', revealMenu);
 
   if (settings.hasSubmenu) {
+    var parentLink = $('.cm-has-submenu');
     var toggleSubMenu = function() {
       $(this).parent().toggleClass('cm-has-submenu-open').find('ul').slideToggle(200);
     };
 
-    settings.submenu.on('click.clearmenu', '.cm-sub-trigger', toggleSubMenu);
+    parentLink.append('<button class="cm-submenu-trigger" type="button" role="button"></button>');
+    settings.submenu.on('click.clearmenu', '.cm-submenu-trigger', toggleSubMenu);
   }
 }
 
