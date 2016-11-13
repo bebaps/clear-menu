bbd4f5806157947b328047a2f10506c583bdb230
Alexander Persky <info@alexanderpersky.com>
2016-11-13 13:52:37 -0700

    Add icon styles for the sub-menu trigger

--- assets/js/clear-menu.js
+++ assets/js/clear-menu.js
@@ -4,19 +4,13 @@
          * Establish the default CSS classes to use.
          */
         var classes = {
-            menu: 'cm-nav', // Class for the menu
             subMenu: 'cm-submenu', // Class for the sub-menus
-            hasSubMenu: 'cm-has-submenu', // Class for a list item that contains a sub-menu
-            hasSubMenuOpen: 'cm-has-submenu-open', // Class for signifying it's sub-menu is visible
-            subMenuTrigger: 'icon-cross', // Class for the trigger to open the sub-menu
-            panel: 'cm-panel', // Class for the panel that contains the menu
             top: 'cm-panel-top', // Class for revealing the panel from the top
             bottom: 'cm-panel-bottom', // Class for revealing the panel from the bottom
             left: 'cm-panel-left', // Class for revealing the panel from the left
             right: 'cm-panel-right', // Class for revealing the panel from the right
             fade: 'cm-panel-fade', // Class for revealing the panel via a fade animation
-            visible: 'cm-panel-open', // Class to signify the panel is active
-            hamburger: 'cm-hamburger' // Class for the hamburger icon
+            visible: 'cm-panel-open' // Class to signify the panel is active
         };
 
         /**
@@ -28,7 +22,7 @@
             reveal: 'top', // How to reveal the panel
             hasSubMenu: true, // Does this menu have sub-menus
             subMenu: '.cm-submenu', // The class/ID of the sub-menu
-            wordpress: false, // WordPress mode, will soon use default WordPress menu classes to target
+            wordpress: false // WordPress mode, will soon use default WordPress menu classes to target
         }, options );
 
         var setUpClearMenu = function() {
@@ -58,14 +52,11 @@
              */
             if ( settings.hasSubMenu ) {
                 var toggleSubMenu = function() {
-                    $( this ).parent().toggleClass( 'cm-has-submenu-open' ).find( 'ul' ).slideToggle( 200 );
+                    $( this ).parent().find( 'ul' ).toggleClass( 'cm-submenu-open' ).slideToggle( 200 );
                 };
 
-                // $(settings.panel).find('> ul ul').addClass(classes.subMenu);
-                $( settings.subMenu )
-                    .parent()
-                    // .addClass( classes.hasSubMenu )
-                    .append( '<i class="sub-trigger">Sub Menu</i>' );
+                $( settings.panel ).find( '> ul ul' ).addClass( classes.subMenu );
+                $( settings.subMenu ).parent().append( '<i class="icon icon-caret"></i>' );
                 $( settings.subMenu ).next().on( 'click.clearmenu', toggleSubMenu );
             }
         }
@@ -77,7 +68,7 @@
             if ( settings.trigger == '.cm-hamburger' ) {
                 $( this ).toggleClass( 'cm-hamburger-open' );
             }
-            $( settings.panel ).toggleClass( 'cm-panel-open' );
+            $( settings.panel ).toggleClass( classes.visible );
         };
 
         $( settings.trigger ).on( 'click.clearmenu', revealMenu );
--- readme.md
+++ readme.md
@@ -6,6 +6,3 @@ A simple mobile menu that is easy in integrate into an existing project.
 - include the `Scss` files or final `CSS` into your project
 - include jQuery before including the `clear-menu.js` file
 - call the `clearmenu()` function on the element that you want to act as the panel
-
-## TODO
-- let JS add the sub-menu parent class, based upon the appropriate setting
