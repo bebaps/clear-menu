31d4fa650b95196be3185366db7785d8866d2442
Alexander Persky <info@alexanderpersky.com>
2016-11-14 01:30:25 -0700

    Add CoffeScript and rewrite the js as .coffee

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,8 @@ var gulp         = require( 'gulp' ),
     reporter     = require( 'postcss-reporter' ),
     cssnext      = require( 'postcss-cssnext' ),
     stylelint    = require( 'gulp-stylelint' ),
-    sass         = require( 'gulp-sass' );
+    sass         = require( 'gulp-sass' ),
+    coffee       = require( 'gulp-coffee' );
 
 var options = {
     browsersync: {
@@ -50,28 +51,39 @@ gulp.task( 'server', [ 'sass' ], function() {
         browserSync.init( options.browsersync )
     }
 
-    gulp.watch( 'assets/scss/*.scss', [ 'sass' ] );
+    gulp.watch( './src/scss/*.scss', [ 'sass' ] );
     gulp.watch( './*.html' ).on( 'change', browserSync.reload );
 } );
 
 // Compile Sass
 gulp.task( 'sass', function() {
     return gulp
-        .src( 'assets/scss/*.scss' )
+        .src( './src/scss/*.scss' )
         .pipe( sourcemaps.init() )
         .pipe( sass( options.sass )
             .on( 'error', sass.logError ) )
         .pipe( autoprefixer( options.autoprefixer ) )
         .pipe( sourcemaps.write( '/' ) )
-        .pipe( gulp.dest( 'assets/css' ) )
+        .pipe( gulp.dest( './dist/css' ) )
         .pipe( browserSync.stream() );
 } );
 
 // Lint Sass/CSS
 gulp.task( 'lint:sass', function() {
     return gulp
-        .src( 'assets/scss/*.scss' )
+        .src( './src/scss/*.scss' )
         .pipe( stylelint( options.stylelint ) );
+} );
+
+// Compile CoffeeScript
+gulp.task( 'js', function() {
+    return gulp
+        .src( './src/coffee/*.coffee' )
+        .pipe( sourcemaps.init() )
+        .pipe( coffee( { bare: true } ) )
+        .pipe( sourcemaps.write( '/' ) )
+        .pipe( gulp.dest( './dist/js' ) )
+        .pipe( browserSync.stream() );
 } );
 
 gulp.task( 'default', [ 'server' ] );
